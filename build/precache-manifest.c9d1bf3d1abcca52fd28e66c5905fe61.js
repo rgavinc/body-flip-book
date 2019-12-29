ef431ff56381501dd6c11a1346d4c28a6d6a1c78
Gavin Robertson <gavinrob92@gmail.com>
2019-12-29 10:15:22 -0700

    added back build folder

---
+++ build/precache-manifest.c9d1bf3d1abcca52fd28e66c5905fe61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f78da51fd117fe6e304ce49cdcbaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "671f44da2f8549f38483",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ff29a72f0b390c12b958",
+    "url": "/static/js/2.fc9a1937.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fc9a1937.chunk.js.LICENSE"
+  },
+  {
+    "revision": "671f44da2f8549f38483",
+    "url": "/static/js/main.76b37f0b.chunk.js"
+  },
+  {
+    "revision": "93ac8fbf3a5f6af125b5",
+    "url": "/static/js/runtime-main.7b2c24c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
